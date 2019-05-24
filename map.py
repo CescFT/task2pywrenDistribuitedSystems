2907c4ba82c298b6d980ee4d18a9367d60845515
Francesc Ferre <francesc.ferre@estudiants.urv.cat>
2019-05-24 11:39:15 +0200

    master: canvis debug

--- map.py
+++ map.py
@@ -26,12 +26,14 @@ def master(N):
     params=pika.URLParameters(urlAMQP)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    channel.queue_declare('cua'+str(0))
-    channel.queue_bind(exchange='logs',queue='cua'+str(0))
+    channel.queue_declare('cua'+str(000))
+    channel.queue_bind(exchange='logs',queue='cua'+str(000))
     v=(int)(random.random() * N + 1)
     channel.basic_publish(exchange='', routing_key='cua'+str(v), body='start')
-    print('master msg send')
+    print(f'master msg send {v}')
     def callback(ch, method, properties, body):
+        bodyr = body.decode('latin1')
+        print(f'master received {bodyr}')
         global comptador
         global v
         comptador=comptador+1
@@ -43,8 +45,8 @@ def master(N):
             v=(int)(random.random() * N + 1)
             print(v)
             channel.basic_publish(exchange='', routing_key='cua'+str(v), body='start')
-            print('master msg sent start')
-    channel.basic_consume(callback, queue='cua'+str(0), no_ack=True)
+            print(f'master msg sent start {v}')
+    channel.basic_consume(callback, queue='cua'+str(000), no_ack=True)
     channel.start_consuming()
     channel.close()
 
@@ -63,8 +65,8 @@ def slave(identificador):
     params=pika.URLParameters(urlAMQP)
     connection = pika.BlockingConnection(params)
     channel = connection.channel()
-    channel.queue_declare('cua'+str(identificador))
-    channel.queue_bind(exchange='logs',queue='cua'+str(identificador))
+    channel.queue_declare('cua'+str(identificador)+'a')
+    channel.queue_bind(exchange='logs',queue='cua'+str(identificador)+'a')
     
        
     def callback(ch, method, properties, body):
@@ -72,13 +74,13 @@ def slave(identificador):
         global resultat
         global id
         primer_missatge=body.decode("latin1")
+        print(f'msg: {primer_missatge}')
         if primer_missatge == 'start':
             #soc el elegit           
             v=(int)(random.random() * 100 + 1)
             print(f'{id} sends {v}')
 #             resultat.append(v)
             channel.basic_publish(exchange='logs', routing_key='', body=str(v))
-            channel.basic_publish(exchange='', routing_key='cua'+str(0), body=str(v))
         elif primer_missatge =='stop':
 #             channel.basic_publish(exchange='logs', routing_key='', body='stop')
             channel.stop_consuming()
@@ -86,7 +88,7 @@ def slave(identificador):
             print(f'{id} received {primer_missatge}')
             resultat.append(primer_missatge)
             
-    channel.basic_consume(callback, queue='cua'+str(identificador), no_ack=True)
+    channel.basic_consume(callback, queue='cua'+str(identificador)+'a', no_ack=True)
     channel.start_consuming()
     channel.close()
     return resultat
